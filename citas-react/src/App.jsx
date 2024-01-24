8a339d7fe21006dbca545f8eac858203ce060dad
luis Arturo Cárdenas Bustamante <luis.cardenas.bustamante@gmail.com>
2024-01-24 14:58:11 -0700

    Subimos los ultimos cambios

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,17 +1,19 @@
+import {useState} from 'react'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import './App.css'
 import ListadoPacientes from './components/ListadoPacientes'
 
+
 function App() {
   // const edad = 18
-
+  const [pacientes, setPacientes] = useState([]);
   return (
     <div className='container mx-auto mt-20'>
       <Header />
       
       <div className="mt-12 md:flex">
-        <Formulario />
+        <Formulario  setPacientes={setPacientes} pacientes = {pacientes}/>
         <ListadoPacientes />
       </div>
 
--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,22 +1,41 @@
 import { useState, useEffect} from 'react'
 
-function Formulario() {
-    const [nombre, setNombre] = useState();
-    const [propietario, setPropietario] = useState()
-    const [email, setEmail] = useState()
-    const [fecha, setFecha] = useState()
-    const [sintomas, setSintomas] = useState()
+function Formulario( {setPacientes, pacientes} ) {
+    const [nombre, setNombre] = useState('');
+    const [propietario, setPropietario] = useState('');
+    const [email, setEmail] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
+    const [error, setError] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         // Validación de Formulario
+        if( [nombre, propietario, email, fecha, sintomas].includes('')) {
+            setError(true)
+        } else {
+            setError(false)
+        }
 
-        
-        console.log("Enviando Formulario")
+        // Objeto de paciente
+        const objetoPaciente = {
+            nombre,
+            propietario,
+            email,
+            fecha,
+            sintomas
+        }
+
+        setPacientes([...pacientes, objetoPaciente])
+
+        // Reiniciar el form
+        setNombre('')
+        setPropietario('')
+        setEmail('')
+        setFecha('')
+        setSintomas('')
     }
-
-
 
     return ( 
         <div className="md:w-1/2 lg:w-2/5 mx-5">
@@ -26,10 +45,16 @@ function Formulario() {
             </p>
 
             <form onSubmit={handleSubmit} className="bg-white shadow-md hover:shadow-2xl rounded-lg py-10 px-5 mb-10">
-                <div className="mb-5">
-                    <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
 
-                    <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                {error && ( 
+                    <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
+                        <p>Todos los campos son obligatorios</p>
+                    </div>
+                )}
+                <div className="mb-5">
+                    <label htmlFor="nombre" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+
+                    <input id="nombre" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
                     value={nombre}
                     onChange={ (e) => setNombre(e.target.value)}/>
                 </div>
@@ -50,11 +75,11 @@ function Formulario() {
                 </div>
 
                 <div className="mb-5">
-                    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Alta</label>
+                    <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold">Alta</label>
 
-                    <input id="alta" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    <input id="fecha" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                     value={fecha}
-                    onChange={ (e) => setAlta(e.target.value)}/>
+                    onChange={ (e) => setFecha(e.target.value)}/>
                 </div>
 
                 <div className="mb-5">
