1e45a0b668c10d6ec29fb4671f5c6bef36133bb8
luis Arturo Cárdenas Bustamante <luis.cardenas.bustamante@gmail.com>
2024-02-09 13:09:06 -0700

    Se termino el primer proyecto con React

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect} from 'react'
+import { useEffect, useState } from 'react'
+import Error from './Error'
 
-function Formulario( {setPacientes, pacientes} ) {
+function Formulario({ setPacientes, pacientes, paciente, setPaciente }) {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
@@ -8,11 +9,30 @@ function Formulario( {setPacientes, pacientes} ) {
     const [sintomas, setSintomas] = useState('');
     const [error, setError] = useState(false);
 
+    useEffect(() => {
+        if (Object.keys(paciente).length > 0) {
+            setNombre(paciente.nombre);
+            setPropietario(paciente.propietario);
+            setEmail(paciente.email);
+            setFecha(paciente.fecha);
+            setSintomas(paciente.sintomas);
+        }
+    }, [paciente])
+
+
+    // Generamos un ID aleatorio por medio de la fecha y el metodo Math de javascript
+    const generarId = () => {
+        const random = Math.random().toString(36).substring(2);
+        const fecha = Date.now().toString(36);
+
+        return random + fecha;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
         // Validación de Formulario
-        if( [nombre, propietario, email, fecha, sintomas].includes('')) {
+        if ([nombre, propietario, email, fecha, sintomas].includes('')) {
             setError(true)
         } else {
             setError(false)
@@ -27,7 +47,19 @@ function Formulario( {setPacientes, pacientes} ) {
             sintomas
         }
 
-        setPacientes([...pacientes, objetoPaciente])
+        if (paciente.id) {
+            // Editando el registro
+            objetoPaciente.id = paciente.id;
+
+            const pacientesActualizados = pacientes.map(pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+
+            setPacientes(pacientesActualizados);
+            setPaciente({});
+        } else {
+            // Nuevo registro
+            objetoPaciente.id = generarId();
+            setPacientes([...pacientes, objetoPaciente])
+        }
 
         // Reiniciar el form
         setNombre('')
@@ -37,7 +69,7 @@ function Formulario( {setPacientes, pacientes} ) {
         setSintomas('')
     }
 
-    return ( 
+    return (
         <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
             <p className="text-lg mt-5 text-center mb-10">
@@ -46,40 +78,36 @@ function Formulario( {setPacientes, pacientes} ) {
 
             <form onSubmit={handleSubmit} className="bg-white shadow-md hover:shadow-2xl rounded-lg py-10 px-5 mb-10">
 
-                {error && ( 
-                    <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-                        <p>Todos los campos son obligatorios</p>
-                    </div>
-                )}
+                {error && <Error>Todos los campos son obligatorios</Error>}
                 <div className="mb-5">
                     <label htmlFor="nombre" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
 
-                    <input id="nombre" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    value={nombre}
-                    onChange={ (e) => setNombre(e.target.value)}/>
+                    <input id="nombre" type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)} />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
 
                     <input id="propietario" type="text" placeholder="Nombre del propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    value={propietario}
-                    onChange={ (e) => setPropietario(e.target.value)}/>
+                        value={propietario}
+                        onChange={(e) => setPropietario(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
 
                     <input id="email" type="email" placeholder="Email del propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    value={email}
-                    onChange={ (e) => setEmail(e.target.value)}/>
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
                     <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold">Alta</label>
 
                     <input id="fecha" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                    value={fecha}
-                    onChange={ (e) => setFecha(e.target.value)}/>
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)} />
                 </div>
 
                 <div className="mb-5">
@@ -90,14 +118,14 @@ function Formulario( {setPacientes, pacientes} ) {
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Describe los sintomas"
                         value={sintomas}
-                        onChange={ (e) => setSintomas(e.target.value)}
+                        onChange={(e) => setSintomas(e.target.value)}
                     />
                 </div>
 
-                <input 
+                <input
                     type="submit"
                     className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-                    value={"Agregar paciente"}
+                    value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
                 />
             </form>
         </div>
--- citas-react/src/components/Paciente.jsx
+++ citas-react/src/components/Paciente.jsx
@@ -1,28 +1,45 @@
-function Pacientes() {
-    return ( 
-            <div className="mx-5 my-10 bg-white hover:shadow-2xl px-5 py-10 rounded-xl ">
-                <p className="font-bold mb-3 text-gray-700 uppercase">
-                    Nombre:{''}
-                    <span className="font-normal normal-case">Hook</span>
-                </p>
-                <p className="font-bold mb-3 text-gray-700 uppercase">
-                    Propietario:{''}
-                    <span className="font-normal normal-case">Luis</span>
-                </p>
-                <p className="font-bold mb-3 text-gray-700 uppercase">
-                    Email:{''}
-                    <span className="font-normal normal-case">correo@correo.com</span>
-                </p>
-                <p className="font-bold mb-3 text-gray-700 uppercase">
-                    Fecha Alta:{''}
-                    <span className="font-normal normal-case">10 Diciembre de 2022</span>
-                </p>
-                <p className="font-bold mb-3 text-gray-700 uppercase">
-                    Nombre:{''}
-                    <span className="font-normal normal-case"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet justo rhoncus, vulputate ex sit amet, ultricies nisl. Nam scelerisque malesuada est, convallis gravida nulla venenatis tincidunt. Quisque pellentesque mauris vel mauris varius, sed finibus nisl ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus ut elit massa. Aenean finibus augue vel sem feugiat ornare. In vitae metus consequat lectus ornare ullamcorper sed eu purus. Mauris dignissim, justo eu vestibulum egestas, dolor nulla lobortis elit, id scelerisque lectus orci sed magna. Aenean et eros sit amet nisi facilisis fermentum ut ut mauris. Morbi porta hendrerit purus, vel pretium odio iaculis in. In finibus semper nibh, id ultrices ipsum scelerisque ac. Suspendisse potenti.</span>
-                </p>
+function Pacientes({ paciente, setPaciente, eliminarPaciente }) {
+    const { nombre, propietario, email, fecha, sintomas, id } = paciente;
+
+    const handleEliminar = () => {
+        // () => eliminarPaciente(id)
+        const respuesta = confirm('Deseas eliminar este paciente?')
+
+        if (respuesta) {
+            eliminarPaciente(id);
+        }
+
+    }
+
+    return (
+        <div className="mx-5 my-10 bg-white hover:shadow-2xl px-5 py-10 rounded-xl ">
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Nombre:{''}
+                <span className="font-normal normal-case">{nombre}</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Propietario:{''}
+                <span className="font-normal normal-case">{propietario}</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Email:{''}
+                <span className="font-normal normal-case">{email}</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Fecha Alta:{''}
+                <span className="font-normal normal-case">{fecha}</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Nombre:{''}
+                <span className="font-normal normal-case">{sintomas}</span>
+            </p>
+
+            <div className="flex justify-between mt-10">
+                <button type="button" className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg" onClick={() => setPaciente(paciente)}>Editar</button>
+                <button type="button" className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg" onClick={handleEliminar}>Eliminar</button>
             </div>
-        );
+        </div>
+    );
 }
 
 export default Pacientes;
